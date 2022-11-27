cbecb1e38d905361ccca583c151dc262a20a5ba5
Dimitrios Apostolakis <dapostolakis@athtech.gr>
2022-11-27 22:33:08 +0200

    Creat Database(Tools->Console->add-migration AddCategoryToDatabase(NuGet Tools) + update-database)

--- FundRaiserWeb/Data/ApplicationDbContext.cs
+++ FundRaiserWeb/Data/ApplicationDbContext.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FundRaiserWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace FundRaiserWeb.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
--- FundRaiserWeb/Models/Category.cs
+++ FundRaiserWeb/Models/Category.cs
@@ -1,6 +1,15 @@
-﻿namespace FundRaiserWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FundRaiserWeb.Models
 {
     public class Category
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+
     }
 }
