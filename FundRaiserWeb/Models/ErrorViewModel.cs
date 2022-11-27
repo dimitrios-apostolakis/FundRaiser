5f49ea9640448f7763ac05c72c790891fe357fb3
Dimitrios Apostolakis <dapostolakis@athtech.gr>
2022-11-27 17:38:53 +0200

    Add project files.

---
+++ FundRaiserWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FundRaiserWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
