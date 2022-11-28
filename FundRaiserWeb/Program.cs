71c35c8bd070372ef19f520f9bc708e480504417
Dimitrios Apostolakis <dapostolakis@athtech.gr>
2022-11-28 23:05:33 +0200

    Add Theme(+Add superhero.css(Bootswatch) +site +layout (?AddRazorRuntimeCompilation))

--- FundRaiserWeb/Program.cs
+++ FundRaiserWeb/Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
-
+//builder.Services.AddRazorPages().AddRazorRuntimeCompilation();//132, app.MapRazorPages()
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
