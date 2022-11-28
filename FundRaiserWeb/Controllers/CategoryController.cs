2352147e829f61252e7e489acd625dfa00e0f448
Dimitrios Apostolakis <dapostolakis@athtech.gr>
2022-11-28 16:23:39 +0200

    Retrieve Categories(+Insert row +Layout +Create Object Category Controller +Retrieve(var) List)

--- FundRaiserWeb/Controllers/CategoryController.cs
+++ FundRaiserWeb/Controllers/CategoryController.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FundRaiserWeb.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FundRaiserWeb.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
         {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {   //retrieve all records, convert to a list, assign to variable
+            var objCategoryList = _db.Categories.ToList();
             return View();
         }
     }
